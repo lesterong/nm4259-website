c4687860cfce2ae563d929b1996e12d5083c8d2c
Lester Ong <lesterong776@gmail.com>
2024-08-16 11:45:43 +0800

    Move to client only

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -1,5 +1,4 @@
 import { LOCALE } from "@config";
-import { useEffect, useState } from "react";
 
 interface DatetimesProps {
   pubDatetime: string | Date;
@@ -17,25 +16,14 @@ export default function Datetime({
   size = "sm",
   className = "",
 }: Props) {
-  const [render, setRender] = useState(false);
-  useEffect(() => {
-    setRender(true);
-  }, []);
-
-  if (!render) {
-    return (
-      <div className={`flex items-center opacity-0 ${className}`.trim()}>
-        <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Loading
-        </span>
-      </div>
-    );
-  }
-
   return (
-    <div className={`flex items-center opacity-80 ${className}`.trim()}>
+    <div
+      className={`flex items-center opacity-80 ${className}`.trim()}
+    >
       {modDatetime && modDatetime > pubDatetime ? (
-        <span className="sr-only">Updated:</span>
+        <span className="sr-only">
+          Updated:
+        </span>
       ) : (
         <span className="sr-only">Published:</span>
       )}
